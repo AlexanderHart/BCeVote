56a1cd36b0f8d605bbf1a95595b578eaf5d75055
Alexander Hart <alexanderhart97@gmail.com>
2019-09-23 19:18:05 -0400

    Removed petition funcationality.
    Doesn't belong in this file; will restructure so petition has it's own blueprint.

--- user/views.py
+++ user/views.py
@@ -22,7 +22,7 @@ from flask import render_template, Blueprint, url_for, \
 from flask.ext.login import login_user, logout_user, \
     login_required, current_user
 
-from project.models import User, Petition
+from project.models import User
 # from project.email import send_email
 from project import db, bcrypt
 from .forms import LoginForm, RegisterForm, ChangePasswordForm, CreatePetitionForm
@@ -153,57 +153,3 @@ def profile():
             flash('Password change was unsuccessful.', 'danger')
             return redirect(url_for('user.profile'))
     return render_template('user/profile.html', form=form)
-
-@user_blueprint.route('/createPetition', methods=['GET', 'POST'])
-@login_required
-@check_confirmed
-def createPetition():
-    form = CreatePetitionForm(request.form)
-    if form.validate_on_submit():
-        # create kmd and algod clients
-        kcl = kmd.KMDClient("43bdd18aee3788a8dd41d39a4d4c20c4e22539ec9c4faf0c3525cd30f0e2baae", "http://127.0.0.1:7833")
-        acl = algod.AlgodClient("44018d81c65b1b2fc4c33380cc9bb3cd0e6c33f8ede8bbee63638943f819b003", "http://127.0.0.1:45291")
-
-        wallet_name = "testWallet"
-        wallet_pswd = "root"
-
-        # get the wallet ID
-        wallets = kcl.list_wallets()
-
-        wallet_id = None
-        for w in wallets:
-    	    if w["name"] == wallet_name:
-                wallet_id = w["id"]
-                break
-
-        # if it doesn't exist, create the wallet and get its ID
-        if not wallet_id:
-            wallet_id = kcl.create_wallet(wallet_name, wallet_pswd)["id"]
-
-        # get a handle for the wallet
-        handle = kcl.init_wallet_handle(wallet_id, wallet_pswd)
-
-        # generate account with account and check if it's valid
-        private_key_1, address_1 = account.generate_account()
-
-        # import generated account into the wallet
-        kcl.import_key(handle, private_key_1)
-
-        petition = Petition(
-            name=form.name.data,
-	    publicKey=address_1,
-            yesCount=0
-        )
-        db.session.add(petition)
-        db.session.commit()
-
-        flash('Petition has been created!.', 'success')
-        #return redirect(url_for("user.unconfirmed"))
-
-    return render_template('user/createPetition.html', form=form)
-
-@user_blueprint.route('/listPetitions', methods=['GET', 'POST'])
-@login_required
-@check_confirmed
-def listPetitions():
-    return render_template('user/listPetitions.html')
