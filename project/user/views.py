37c8806748a4e1b25c057e6821c6d5f677ede6d7
Alexander Hart <alexanderhart97@gmail.com>
2019-10-15 11:22:34 -0400

    Had the wrong code in the wrong file.

--- project/user/views.py
+++ project/user/views.py
@@ -11,8 +11,6 @@ from algosdk import algod
 from algosdk import account
 from algosdk import mnemonic
 import json
-import base64
-import hashlib
 
 
 from project.email import send_email
@@ -24,229 +22,134 @@ from flask import render_template, Blueprint, url_for, \
 from flask.ext.login import login_user, logout_user, \
     login_required, current_user
 
-from project.models import User, Petition
+from project.models import User
 # from project.email import send_email
 from project import db, bcrypt
-from .forms import LoginForm, RegisterForm, ChangePasswordForm, CreatePetitionForm, ListPetitionForm
+from .forms import LoginForm, RegisterForm, ChangePasswordForm, CreatePetitionForm
 
 import datetime
-import time
 
 ################
 #### config ####
 ################
 
 user_blueprint = Blueprint('user', __name__,)
-petition_blueprint = Blueprint('petition', __name__,)
 
-kcl = kmd.KMDClient("780954d4d6d9a6e052d92f4b6c91c5f8f044514cabe1c697b3904270c784fd75", "http://127.0.0.1:7833")
-acl = algod.AlgodClient("4a327d8ae7faa2d890ff05d95bae2a167b7bdff2e58630dd4afff1f693856d90", "http://127.0.0.1:8080")
 
 ################
 #### routes ####
 ################
 
-@petition_blueprint.route('/createPetition', methods=['GET', 'POST'])
-@login_required
-@check_confirmed
-def createPetition():
-    form = CreatePetitionForm(request.form)
+@user_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
     if form.validate_on_submit():
-
-        petitionWallet = "Petitions"
-        petitionWalletPassword = "root"
-
-        masterAccountWallet = "MasterAccounts"
-        masterAccountWalletPassword = "root"
-
-
-        # get the wallet ID
-        wallets = kcl.list_wallets()
-
-        petitionWalletID = None
-        for w in wallets:
-    	    if w["name"] == petitionWallet:
-                petitionWalletID = w["id"]
-                break
-
-        masterAccountWalletID = None
-        for w2 in wallets:
-    	    if w2["name"] == masterAccountWallet:
-                masterAccountWalletID = w2["id"]
-                break
-
-        # if it doesn't exist, create the wallet and get its ID
-        if not petitionWalletID:
-            petitionWalletID = kcl.create_wallet(petitionWallet, petitionWalletPassword)["id"]
-
-
-        # get a handle for the wallet
-        handle = kcl.init_wallet_handle(petitionWalletID, petitionWalletPassword)
-
-        #### MASTER ACCOUNTS
-        # if it doesn't exist, create the wallet and get its ID
-        if not masterAccountWalletID:
-            masterAccountWalletID = kcl.create_wallet(masterAccountWallet, masterAccountWalletPassword)["id"]
-
-        # get a handle for the wallet
-        handle2 = kcl.init_wallet_handle(masterAccountWalletID, masterAccountWalletPassword)
-
-
-
-        # generate account with account and check if it's valid
-        private_key_1, address_1 = account.generate_account()
-
-        # generate master account with account and check if it's valid
-        private_key_2, address_2 = account.generate_account()
-
-
-
-        # import generated account into the wallet
-        kcl.import_key(handle, private_key_1)
-        kcl.import_key(handle2, private_key_2)
-
-        petition = Petition(
-            name=form.name.data,
-	    publicKey=address_1,
-        masterAccount = address_2,
-            yesCount=0
+        user = User(
+            email=form.email.data,
+            password=form.password.data,
+            confirmed=False
         )
-        db.session.add(petition)
+        db.session.add(user)
         db.session.commit()
 
-        flash('Petition has been created!.', 'success')
+        token = generate_confirmation_token(user.email)
+        confirm_url = url_for('user.confirm_email', token=token, _external=True)
+        html = render_template('user/activate.html', confirm_url=confirm_url)
+        subject = "Please confirm your email"
+        send_email(user.email, subject, html)
 
-    return render_template('petition/createPetition.html', form=form)
+        login_user(user)
 
+        flash('A confirmation email has been sent via email.', 'success')
+        return redirect(url_for("user.unconfirmed"))
 
-def DoubleVoteChecker(curUser, txArray):
-    print("Total number of Tx: " + str(len(txArray)))
-    for i in range(0,len(txArray)):
-        currentTx = txArray.get("transactions")[i]
-        # read noteb64 field of first transaction
-        noteb64 = currentTx.get("noteb64")
-        # Decode to bytes
-        note = base64.b64decode(noteb64)
-        email = json.loads(note)["email"]
-        if email == str(curUser):
-            return True
-            break
-    return False
+    return render_template('user/register.html', form=form)
 
 
-@petition_blueprint.route('/listPetitions', methods=['GET', 'POST'])
+@user_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(
+                user.password, request.form['password']):
+            login_user(user)
+            flash('Welcome.', 'success')
+            return redirect(url_for('main.home'))
+        else:
+            flash('Invalid email and/or password.', 'danger')
+            return render_template('user/login.html', form=form)
+    return render_template('user/login.html', form=form)
+
+@user_blueprint.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You were logged out.', 'success')
+    return redirect(url_for('user.login'))
+
+
+@user_blueprint.route('/confirm/<token>')
+@login_required
+def confirm_email(token):
+    try:
+        email = confirm_token(token)
+    except:
+        flash('The confirmation link is invalid or has expired.', 'danger')
+    user = User.query.filter_by(email=email).first_or_404()
+    if user.confirmed:
+        flash('Account already confirmed. Please login.', 'success')
+    else:
+        user.confirmed = True
+        user.confirmed_on = datetime.datetime.now()
+        db.session.add(user)
+        db.session.commit()
+        flash('You have confirmed your account. Thanks!', 'success')
+
+        #################################################################################
+        ## This wold be a good place to implement the user's blockchain creds initiation.
+        ##
+        ## Required functions from Algorand SDK.
+        ## handle = kcl.init_wallet_handle(wallet_id, wallet_pswd)
+        ## private_key_1, address_1 = account.generate_account()
+        ##
+        ##
+        #################################################################################
+
+    return redirect(url_for('main.home'))
+
+@user_blueprint.route('/resend')
+@login_required
+def resend_confirmation():
+    token = generate_confirmation_token(current_user.email)
+    confirm_url = url_for('user.confirm_email', token=token, _external=True)
+    html = render_template('user/activate.html', confirm_url=confirm_url)
+    subject = "Please confirm your email"
+    send_email(current_user.email, subject, html)
+    flash('A new confirmation email has been sent.', 'success')
+    return redirect(url_for('user.unconfirmed'))
+
+@user_blueprint.route('/unconfirmed')
+@login_required
+def unconfirmed():
+    if current_user.confirmed:
+        return redirect('main.home')
+    flash('Please confirm your account!', 'warning')
+    return render_template('user/unconfirmed.html')
+
+@user_blueprint.route('/profile', methods=['GET', 'POST'])
 @login_required
 @check_confirmed
-def listPetitions():
-    form = ListPetitionForm(request.form)
+def profile():
+    form = ChangePasswordForm(request.form)
     if form.validate_on_submit():
-        if request.method == 'POST':
-            petPK = str(request.form['voteYes'])
-            petitionMasterAccount = (Petition.query.filter_by(publicKey=petPK).one())
-            # check if the current email has been used
-            # for petition before.
-            print("public key: " + petPK)
-            txs = acl.transactions_by_address(petPK)
-            if DoubleVoteChecker(current_user.email, txs):
-                flash(str(current_user.email) + " has already signed this petition.")
-            else:
-                masterAccountWallet = "MasterAccounts"
-                masterAccountPassword = "root"
-                masterAccount = petitionMasterAccount.masterAccount
-                print(masterAccount)
-
-                wallets = kcl.list_wallets()
-                masterAccountID = None
-                for w in wallets:
-                    if w["name"] == masterAccountWallet:
-                        masterAccountID = w["id"]
-                        break
-
-                masterAccountHandle = kcl.init_wallet_handle(masterAccountID,masterAccountPassword)
-
-                petitionWallet = "Petitions"
-                petitionWalletPassword = "root"
-
-                petitionWalletID = None
-                for w in wallets:
-                    if w["name"] == petitionWallet:
-                        petitionWalletID = w["id"]
-                        break
-
-                if not petitionWalletID:
-                    petitionWalletID = kcl.create_wallet(petitionWallet, petitionWalletPassword)["id"]
-
-                handle = kcl.init_wallet_handle(petitionWalletID, petitionWalletPassword)
-                params = acl.suggested_params()
-                gen = params["genesisID"]
-                gh = params["genesishashb64"]
-                last_round = params["lastRound"]
-                fee = params["fee"]
-                #jsonInput = '{"email": "' + str(hashlib.sha256(current_user.email.encode()).hexdigest()) + '", "timeStamp": "34123213124.32412"}'
-                jsonInput = '{"email": "' + str(current_user.email) + '", "timeStamp": "' + str(time.time()) + '"}'
-                note = (jsonInput).encode()
-                amount = 103000
-                txn = transaction.PaymentTxn(masterAccount, fee, last_round, last_round+100, gh, petPK, amount, gen=gen, note=note)
-                signed_with_kmd = kcl.sign_transaction(masterAccountHandle,masterAccountPassword, txn)
-                private_key = kcl.export_key(masterAccountHandle, masterAccountPassword,masterAccount)
-                signed_offline = txn.sign(private_key)
-                transaction_id = acl.send_transaction(signed_with_kmd)
-                print("Tx ID: " + str(transaction_id))
-            # if request.form["details"]:
-            #     curPetition = Petition.query.filter_by(uid=str(request.form["details"])).first()
-            #     print(curPetition)
-            #     return render_template('petition/viewDetails.html', curPetition=curPetition)
-            # elif request.form["voteYes"]:
-            #     petPK = str(request.form['voteYes'])
-            #     # to check if the current email has been used
-            #     # for petition before.
-            #     txs = acl.transactions_by_address(petPK)
-            #     if DoubleVoteChecker(current_user.email, txs):
-            #         flash(str(current_user.email) + " has already signed this petition.")
-            #     else:
-            #         flash("This user is allowed to sign this peition.")
-            #         masterAccountWallet = "MasterAccounts"
-            #         masterAccountPassword = "root"
-            #         masterAccount = "N5EDLDXPWPAW4STCP24OOOLNSLSGXOI3RUUHXMDOTNMK252M3CH6CT7OJE"
-            #
-            #         wallets = kcl.list_wallets()
-            #         masterAccountID = None
-            #         for w in wallets:
-            #             if w["name"] == masterAccountWallet:
-            #                 masterAccountID = w["id"]
-            #                 break
-            #
-            #         masterAccountHandle = kcl.init_wallet_handle(masterAccountID,
-            #                              masterAccountPassword)
-            #
-            #         petitionWallet = "Petitions"
-            #         petitionWalletPassword = "root"
-            #
-            #         petitionWalletID = None
-            #         for w in wallets:
-            #             if w["name"] == petitionWallet:
-            #                 petitionWalletID = w["id"]
-            #                 break
-            #
-            #         if not petitionWalletID:
-            #             petitionWalletID = kcl.create_wallet(petitionWallet, petitionWalletPassword)["id"]
-            #
-            #         handle = kcl.init_wallet_handle(petitionWalletID, petitionWalletPassword)
-            #         params = acl.suggested_params()
-            #         gen = params["genesisID"]
-            #         gh = params["genesishashb64"]
-            #         last_round = params["lastRound"]
-            #         fee = params["fee"]
-            #         note = str(current_user.email).encode()
-            #         amount = 100000
-            #         txn = transaction.PaymentTxn(masterAccount, fee, last_round, last_round+100, gh, petPK, amount, gen=gen, note=note)
-            #         signed_with_kmd = kcl.sign_transaction(masterAccountHandle,masterAccountPassword, txn)
-            #         private_key = kcl.export_key(masterAccountHandle, masterAccountPassword,masterAccount)
-            #         signed_offline = txn.sign(private_key)
-            #         transaction_id = acl.send_transaction(signed_with_kmd)
-            # elif request.form["voteNo"]:
-            #     print("r2r2ef")
-
-
-    myPetition = Petition.query.all()
-    return render_template('petition/listPetitions.html', form=form, myPetition=myPetition)
+        user = User.query.filter_by(email=current_user.email).first()
+        if user:
+            user.password = bcrypt.generate_password_hash(form.password.data)
+            db.session.commit()
+            flash('Password successfully changed.', 'success')
+            return redirect(url_for('user.profile'))
+        else:
+            flash('Password change was unsuccessful.', 'danger')
+            return redirect(url_for('user.profile'))
+    return render_template('user/profile.html', form=form)
